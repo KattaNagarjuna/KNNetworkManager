586aef29c5b62b68b42efe27b731f5c583022548
Nagarjuna Katta <nagarjunakatta@MIN-0714-162s-MacBook-Pro.local>
2016-02-08 16:59:32 +0530

    added network project

---
+++ KNNetworkManager/KNNetworkManager/KNNetworkManager.h
@@ -0,0 +1,19 @@
+//
+//  KNNetworkManager.h
+//  KNNetworkManager
+//
+//  Created by Nagarjuna Katta on 08/02/16.
+//  Copyright © 2016 katta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KNNetworkManager.
+FOUNDATION_EXPORT double KNNetworkManagerVersionNumber;
+
+//! Project version string for KNNetworkManager.
+FOUNDATION_EXPORT const unsigned char KNNetworkManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KNNetworkManager/PublicHeader.h>
+
+
